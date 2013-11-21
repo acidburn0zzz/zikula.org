cc9f1c1d5fad09d4afa9b2409806ebf42a465341
Craig Heydenburg <craigh@mac.com>
2013-11-21 16:53:27 -0500

    make sure document loaded for js

--- Resources/public/js/modern-business.js
+++ Resources/public/js/modern-business.js
@@ -1,9 +1,11 @@
-// Activates the Carousel
-jQuery('.carousel').carousel({
-  interval: 5000
-})
+jQuery(document).ready(function() {
+    // Activates the Carousel
+    jQuery('.carousel').carousel({
+      interval: 5000
+    });
 
-// Activates Tooltips for Social Links
-jQuery('.tooltip-social').tooltip({
-  selector: "a[data-toggle=tooltip]"
-})
+    // Activates Tooltips for Social Links
+    jQuery('.tooltip-social').tooltip({
+      selector: "a[data-toggle=tooltip]"
+    });
+});
