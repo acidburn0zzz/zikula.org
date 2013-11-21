e1ec631757c459f753aa5cf9b6451329336f6661
Craig Heydenburg <craigh@mac.com>
2013-11-20 19:21:26 -0500

    add new home template with js file

---
+++ Resources/public/js/modern-business.js
@@ -0,0 +1,9 @@
+// Activates the Carousel
+jQuery('.carousel').carousel({
+  interval: 5000
+})
+
+// Activates Tooltips for Social Links
+jQuery('.tooltip-social').tooltip({
+  selector: "a[data-toggle=tooltip]"
+})
